43af54ae170a70019dc20120f610234e7a9a8f6e
tanxin <tanxin@bbktel.com>
2019-04-12 16:16:01 +0800

    feat(icon): icon组件支持大于0xFFFF的图标编码或者字符

--- src/components/icon/index.vue
+++ src/components/icon/index.vue
@@ -3,6 +3,35 @@
 </template>
 
 <script>
+  function unicodes2string () {
+    var codeUnits = [], codeLen = 0, result = "";
+
+    for (var index = 0, len = arguments.length; index !== len; ++index) {
+      var codePoint = +arguments[index];
+      // correctly handles all cases including `NaN`, `-Infinity`, `+Infinity`
+      // The surrounding `!(...)` is required to correctly handle `NaN` cases
+      // The (codePoint>>>0) === codePoint clause handles decimals and negatives
+      if (!(codePoint < 0x10FFFF && (codePoint >>> 0) === codePoint))
+        throw RangeError("Invalid code point: " + codePoint);
+      if (codePoint <= 0xFFFF) { // BMP code point
+        codeLen = codeUnits.push(codePoint);
+      } else { // Astral code point; split in surrogate halves
+        // https://mathiasbynens.be/notes/javascript-encoding#surrogate-formulae
+        codePoint -= 0x10000;
+        codeLen = codeUnits.push(
+          (codePoint >> 10) + 0xD800,  // highSurrogate
+          (codePoint % 0x400) + 0xDC00 // lowSurrogate
+        );
+      }
+      if (codeLen >= 0x3fff) {
+        result += String.fromCharCode.apply(null, codeUnits);
+        codeUnits.length = 0;
+      }
+    }
+
+    return result + String.fromCharCode.apply(null, codeUnits);
+  }
+
   export default {
     name: 'vui-icon',
     props: {
@@ -17,7 +46,7 @@
     },
     computed: {
       value() {
-        return String.fromCharCode(parseInt(this.type, 16))
+        return unicodes2string(parseInt(this.type, 16))
       }
     }
   }
