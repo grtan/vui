09fb1aeb2532cbbd15c17adb3f8e810c1007ea3b
weixing <11101493@bbktel.com>
2020-11-23 15:33:46 +0800

    feat: 新增link链接组件

---
+++ src/modules/link/component.vue
@@ -0,0 +1,77 @@
+<template>
+  <span
+    :class="[
+      'vui-link',
+      {
+        [`vui-link--${vue}`]: ['primary', 'regular', 'success', 'fail'].includes(vue),
+        [`vui-link--${size}`]: ['big', 'small', 'mini'].includes(size)
+      }
+    ]"
+    :disabled="disabled"
+    :href="href"
+    :underline="underline"
+    @click="!disabled && emit('click')"
+  >
+    <div v-if="icon && iconPosition === 'before'">icon</div>
+    <slot></slot>
+    <div v-if="icon && iconPosition === 'after'">icon</div>
+  </span>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component({
+  name: 'VuiLink'
+})
+export default class VComponent extends Vue {
+  // 色调
+  @Prop({
+    type: String,
+    default: 'primary'
+  })
+  readonly vue!: 'primary' | 'regular' | 'success' | 'fail'
+
+  // 下划线
+  @Prop({
+    type: Boolean,
+    default: true
+  })
+  readonly underline!: boolean
+
+  // 是否禁用
+  @Prop({
+    type: Boolean,
+    default: false
+  })
+  readonly disabled!: boolean
+
+  // 大小
+  @Prop({
+    type: String,
+    default: 'regular'
+  })
+  readonly size!: 'big' | 'regular' | 'small' | 'mini'
+
+  // 链接
+  @Prop({
+    type: String,
+    default: ''
+  })
+  readonly href!: string
+
+  // 图标名称
+  @Prop({
+    type: String,
+    default: ''
+  })
+  readonly icon?: string
+
+  // 图标位置
+  @Prop({
+    type: String,
+    default: 'before'
+  })
+  readonly iconPosition!: 'before' | 'after'
+}
+</script>
---
+++ src/modules/link/demo/demo1.vue
@@ -0,0 +1,45 @@
+<template>
+  <div :class="$style.root">
+    <div :class="$style.item">
+      <vui-link vue="primary" href="www.baidu.com" :underline="false">无下划线</vui-link>
+      <vui-link vue="success" href="www.baidu.com" :underline="false">无下划线</vui-link>
+      <vui-link vue="fail" href="www.baidu.com">有下划线</vui-link>
+      <vui-link vue="regular" href="www.baidu.com">有下划线</vui-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import VuiLink from '../component.vue'
+
+@Component({
+  components: {
+    VuiLink
+  }
+})
+export default class VComponent extends Vue {
+  static title = '链接下划线'
+}
+</script>
+
+<style lang="scss" module>
+.root {
+  align-items: flex-end;
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: -40px;
+}
+
+.item {
+  align-items: flex-start;
+  display: flex;
+  flex: none;
+  flex-direction: row;
+  margin-top: 40px;
+
+  &:not(:first-child) {
+    margin-left: 50px;
+  }
+}
+</style>
