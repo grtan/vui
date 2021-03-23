20f5d2b93370baa881332cc6786a75c1f3dd3690
谭新 <xin.tan@vivo.com>
2021-03-23 16:16:13 +0800

    fix: 给videojs-hls-quality-selector插件添加hlsQualitySelector属性声明

--- src/types/module.d.ts
+++ src/types/module.d.ts
@@ -34,6 +34,12 @@ declare module 'vue/types/options' {
   }
 }
 
+declare module 'video.js' {
+  interface VideoJsPlayer {
+    hlsQualitySelector: () => any
+  }
+}
+
 export interface VuiComponent {
   install: PluginFunction<any>
 }
